32aefbee26515d47f0004bce2e6567f1f0b40d31
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-02 15:47:16 -0300

    criado: gerenciador de contexto

--- app/app.py
+++ app/app.py
@@ -1,9 +1,16 @@
-import logging 
+import logging
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 logger = logging.getLogger()
 
-app = FastAPI()
+@asynccontextmanager 
+async def lifespan(app):
+    logger.info('Iniciando app.')
+    yield
+    logger.info('Finalizando app.')
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get('/check')
