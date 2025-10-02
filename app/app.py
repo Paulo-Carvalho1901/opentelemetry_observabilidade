52fc2639703ace030b4aa33d0c75552fe520a7d0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-02 15:54:38 -0300

    criando parametros para os logs

--- app/app.py
+++ app/app.py
@@ -3,6 +3,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler)
 
 @asynccontextmanager 
 async def lifespan(app):
